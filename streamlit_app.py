344f7813257a412ff9f6ba8a9e0116500b4a2cca
Christina LaPane <70347084+christinalapane@users.noreply.github.com>
2023-08-28 16:13:40 -0400

    Update streamlit_app.py
    lesson 9

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas 
-import requests
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -23,6 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response 
-streamlit.header('Fruityvice Fruit Advice')
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
